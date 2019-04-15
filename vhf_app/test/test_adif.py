34ac8c4f3be683ddd5c2689252a2c1c5f5e2c990
Jakub Ondrusek <yak@gmx.co.uk>
2019-04-16 00:17:30 +0200

    improved adif tests

--- vhf_app/test/test_adif.py
+++ vhf_app/test/test_adif.py
@@ -12,23 +12,25 @@ class TestAdif(unittest.TestCase):
 
     def _get_adif(self, example):
         return os.path.join(os.path.dirname(__file__), EXAMPLES_DIR, example)
+    
+    def setUp(self):
+        self.adif1 = self._get_adif('PA OM1AKU.adi')
+        self.adif2 = self._get_adif('20190317_OM1WS.adif')
 
     def test_adif(self):
         a = Adif()
         self.assertIsInstance(a, Adif, 'new object is an instance of Adif')
     
     def test_adif_from_file(self):
-       
-        adif_file = self._get_adif('PA OM1AKU.adi')
-        self.assertTrue(os.path.isfile(adif_file), "Check if %s exists." % adif_file)
-        adif_log = Adif(from_file=adif_file)
+        self.assertTrue(os.path.isfile(self.adif1), "Check if %s exists." % self.adif1)
+        adif_log = Adif(from_file=self.adif1)
         self.assertIsInstance(adif_log, Adif, 'Create ADIF log from file')
         self.assertEqual(len(adif_log.qsos), 31, 'Correct number of processed QSOs')
 
     def test_adif_header(self):
         # we should be able to extract a header out of the adif if available
-        adif1 = Adif(from_file=self._get_adif('PA OM1AKU.adi'))
-        adif2 = Adif(from_file=self._get_adif('20190317_OM1WS.adif'))
+        adif1 = Adif(from_file=self.adif1)
+        adif2 = Adif(from_file=self.adif2)
 
         self.assertIsInstance(adif1.header, dict)
         self.assertIsInstance(adif2.header, dict)
@@ -40,8 +42,7 @@ class TestAdif(unittest.TestCase):
         self.assertEqual(adif.comments.get('PWWLO'), 'JN88OJ')
 
     def test_adif_guess(self):
-        adif_file = self._get_adif('PA OM1AKU.adi')
-        with open(adif_file) as f:
+        with open(self.adif1) as f:
             data = f.read()
             self.assertTrue(Adif.can_process(data))
 
