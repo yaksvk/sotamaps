41685dad73bd04d47a2baa5f97fcf05a0bf44bd8
Jakub Ondrusek <yak@gmx.co.uk>
2019-04-16 00:41:27 +0200

    test cases for edi

---
+++ vhf_app/test/test_edi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import unittest
+import os.path
+
+from vhf.edit import Edi
+
+# adif file examples dir (relative to this test file)
+EXAMPLES_DIR = 'examples'
+
+class TestEdi(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
